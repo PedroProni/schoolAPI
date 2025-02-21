8784217667c61713c78103d798809e7018c00b52
Pedro Proni <pedro_proni@hotmail.com>
2025-02-21 11:22:29 -0300

    added verification for file type

--- src/controllers/Photo.js
+++ src/controllers/Photo.js
@@ -1,6 +1,25 @@
+import multer from "multer";
+import multerConfig from "../config/multer";
+
+const upload = multer(multerConfig).single("photo");
+
+
 class Photo {
   async create(req, res) {
-    res.json(req.file);
+    upload(req, res, (err) => {
+      if (err) {
+        if(err.code === "LIMIT_UNEXPECTED_FILE") {
+          return res.status(400).json({
+            errors: ["Invalid file type"],
+          });
+        }
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
--- src/routes/photo.js
+++ src/routes/photo.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
-
 import photo from '../controllers/Photo';
-import multerConfig from '../config/multer';
 
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('photo'), photo.create);
+router.post('/', photo.create);
 
 export default router;
