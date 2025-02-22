a3693f2cf9a909937496e299ca3788823c9087e2
Proni <pedro_proni@hotmail.com>
2025-02-22 08:26:10 -0300

    deploy ready

--- src/config/app.js
+++ src/config/app.js
@@ -1,3 +1,3 @@
 export default {
-  url: "http://localhost:3008",
+  url: process.env.APP_URL,
 };
---
+++ src/server.js
@@ -0,0 +1,5 @@
+import app from './app.js';
+
+const PORT = process.env.APP_PORT;
+
+app.listen(PORT);
